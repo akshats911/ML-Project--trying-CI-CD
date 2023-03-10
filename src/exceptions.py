74855725400e9e32a04cf39aeaebc01d4b56e1ed
Akshat Sharma <93038242+akshats911@users.noreply.github.com>
2023-03-10 22:09:39 +0530

    logging

---
+++ src/exceptions.py
@@ -0,0 +1,16 @@
+import sys
+
+def error_message_detials(error, error_detail:sys):
+    _,_,tb = error_detail.exc_info()
+    file_name = tb.tb_frame.f_code.file_name
+    error_message = f"Error occured in: {file_name} | Line no: {tb.tb_lineno} |\n {str(error)}"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super.__init__(error_message)
+        self.error_message = error_message_detials(error_message, error_detail)
+    
+    def __str__(self):
+        return self.error_message
