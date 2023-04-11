5964c2fe62ebf478035ccf28e9d360f32f9ac2c0
Akshat Sharma <93038242+akshats911@users.noreply.github.com>
2023-04-12 00:00:17 +0530

    added model trainer

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 from src.exceptions import CustomException
 import dill
 
@@ -13,6 +14,23 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as f:
             dill.dump(obj, f)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_models(X_train, Y_train, X_test, Y_test, models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, Y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_score = r2_score(Y_train, y_train_pred)
+            test_score = r2_score(Y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_score
+        return report
+
     except Exception as e:
         raise CustomException(e,sys)
 
